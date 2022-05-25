18f0cee0a3ea4521943a4143753b2cc269d71a23
NotSoPerfect <100975793+NotSoPerfect0@users.noreply.github.com>
2022-05-25 22:57:51 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const header = document.getElementById("bar-heading");
+const addBtn = document.getElementById("add");
+const ideaList = document.getElementsByTagName("li");
+const ideas = [];
+
+var clicks = 1;
+
+header.addEventListener("click", function () {
+    if (clicks < 5) {
+        clicks++;
+    } else {
+        clicks = 1;        
+        window.open("stop.html", "_blank")
+    }
+})
+
+addBtn.addEventListener("click", function() {
+    alert("Work In Progress"); 
+})
